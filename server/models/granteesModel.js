2afcf57463c5558648f35321224b26013da60e79
majdaburas5 <aburasm54@gmail.com>
2023-03-20 16:24:20 +0200

    schema built

---
+++ server/models/granteesModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const granteeSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  picture: String,
+  description: String,
+  aboutMe: String,
+  city: String,
+  country: String,
+  balance: Number,
+  email: String,
+  supporters: [],
+});
+
+const Grantee = mongoose.model("grantee", granteeSchema);
+
+module.exports = Grantee;

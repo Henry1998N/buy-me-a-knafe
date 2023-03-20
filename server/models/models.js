cf91234ccdc5532a7cec8746d0af28fdc46f8c47
majdaburas5 <aburasm54@gmail.com>
2023-03-20 16:26:33 +0200

    add schema

--- server/models/models.js
+++ server/models/models.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Schema = new Schema({});
+const granteeSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  picture: String,
+  description: String,
+  aboutMe: String,
+  city: String,
+  country: String,
+  balance: Number,
+  email: String,
+  supporters: [],
+});
 
-const Weather = mongoose.model("weather", Schema);
+const Grantee = mongoose.model("grantee", granteeSchema);
 
-module.exports = Weather;
+module.exports = Grantee;

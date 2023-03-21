fe3d9b177848cbd78c5bd351aec754632b8d5031
tamem waked <tamem054@gmail.com>
2023-03-21 17:19:03 +0200

    add new mockdata with hd picture

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const api = require("./server/routes/api");
 const path = require("path");
-
+const testApi = require("./server/routes/testApi");
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/buyMeAknafehDB", {
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "node_modules")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", api);
-
+app.use("/test", testApi);
 const port = 3000;
 app.listen(port, function () {
   console.log(`Running on port ${port}`);
--- server/models/granteesModel.js
+++ server/models/granteesModel.js
@@ -2,12 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Supporter = require("./supporterModel");
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/buyMeAknafehDB", {
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("conneted to DB"))
-  .catch((err) => console.log(err));
 const granteeSchema = new Schema({
   firstName: String,
   lastName: String,

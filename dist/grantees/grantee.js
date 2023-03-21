d80ec290496f13e18fd05d60afd2b88480eae062
Zeinab Esawe <zeinab.esawe@gmail.com>
2023-03-21 11:18:18 +0200

    added donation calculate sum

--- dist/grantees/grantee.js
+++ dist/grantees/grantee.js
@@ -1,8 +1,15 @@
+
 class Grantee {
   constructor(id, link, title, description) {
-    this.id = id;
-    this.link = link;
-    this.title = title;
-    this.description = description;
+  this.id = id;
+  this.link = link;
+  this.title = title;
+  this.description = description;
   }
 }
+const KNAFEPRICE = 20
+
+$('.amount-form').on('click', ".btn" , function(){
+  let amount = parseInt($(this).text());
+  $('.submit-btn').text(`Support ${amount * KNAFEPRICE}₪`);
+})

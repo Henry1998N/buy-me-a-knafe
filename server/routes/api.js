4c3792169fb3d9c40c0e359e648bc46bb9054d59
Henry1998N <henry.nur137@gmail.com>
2023-03-21 15:29:39 +0200

    fixed post route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-
+const Supporter = require("../models/supporterModel");
 const Grantee = require("../models/granteesModel");
 
 const { log } = require("handlebars");
@@ -30,15 +30,37 @@ const getSupportersDonations = async function (id) {
       return sum;
     });
 };
+const genrateSupporter = function (supporter) {
+  const newSupporter = new Supporter({
+    name: supporter.name,
+    amount: supporter.amount,
+    message: supporter.message,
+    picture: supporter.picture,
+    date: new Date(),
+  });
+  return newSupporter;
+};
 router.post("/supporter", async function (req, res) {
   const granteeId = req.query.granteeId;
   const supporter = req.body.supporter;
-  Grantee.findById(granteeId).then(async (grantee) => {
-    grantee.supporters.push(supporter);
+  const newSupporter = genrateSupporter(supporter);
+  Grantee.update(
+    { _id: granteeId },
+    { $push: { supporters: newSupporter } }
+  ).then(async () => {
     const granteeBalance = await getSupportersDonations(granteeId);
-    grantee.balance = granteeBalance;
-    res.send(grantee);
+    Grantee.findByIdAndUpdate(granteeId, { balance: granteeBalance }).then(
+      () => {
+        res.send({ msg: "updated" });
+      }
+    );
   });
+  //   Grantee.findByIdAndUpdate(granteeId,{{balance:granteeBalance},{}})
+  //   // Grantee.findById(granteeId).then(async (grantee) => {
+  //   //   grantee.supporters.push(supporter);
+  //   //   grantee.balance = granteeBalance;
+  //   //   res.send(grantee);
+  //   // });
 });
 
 module.exports = router;

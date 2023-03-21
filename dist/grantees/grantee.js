b3a7db98382054213e694e908c18d4beff01a624
Henry1998N <henry.nur137@gmail.com>
2023-03-21 12:10:33 +0200

    added getid function from url

--- dist/grantees/grantee.js
+++ dist/grantees/grantee.js
@@ -1,15 +1,21 @@
-
 class Grantee {
   constructor(id, link, title, description) {
-  this.id = id;
-  this.link = link;
-  this.title = title;
-  this.description = description;
+    this.id = id;
+    this.link = link;
+    this.title = title;
+    this.description = description;
   }
 }
-const KNAFEPRICE = 20
+const KNAFEPRICE = 20;
 
-$('.amount-form').on('click', ".btn" , function(){
+$(".amount-form").on("click", ".btn", function () {
   let amount = parseInt($(this).text());
-  $('.submit-btn').text(`Support ${amount * KNAFEPRICE}₪`);
-})
+  $(".submit-btn").text(`Support ${amount * KNAFEPRICE}₪`);
+});
+
+const getIdFromUrl = function () {
+  const url = window.location.href;
+  const id = url.slice(url.indexOf("=") + 1);
+  console.log(id);
+};
+getIdFromUrl();

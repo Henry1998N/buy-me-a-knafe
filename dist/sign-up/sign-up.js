d34c19aa2ded51a716882182cabf5c23e2a3c05d
Henry1998N <henry.nur137@gmail.com>
2023-03-22 23:19:31 +0200

    fixed login button and signup button

--- dist/sign-up/sign-up.js
+++ dist/sign-up/sign-up.js
@@ -1,10 +1,8 @@
-
-
 $(".cancelbtn").on("click", function () {
   window.location.href = "/homepage/index.html";
 });
 
-$("form.sign-up-form").on("submit", function () {
+$(".sign-up-form").on("submit", function () {
   let firstName = $(".f-nameInput").val();
   let lastName = $(".l-nameInput").val();
   let picture = $(".pictureInput").val();
@@ -16,22 +14,20 @@ $("form.sign-up-form").on("submit", function () {
   let password = $(".passwordInput").val();
   let quote = $(".quoteInput").val();
 
-  $.post(
-    "/signUp",
-    {
-      firstName: firstName,
-      lastName: lastName,
-      picture: picture,
-      description: description,
-      aboutMe: aboutMe,
-      city: city,
-      country: country,
-      balance: 0,
-      email: email,
-      password: password,
-      supporters: [],
-      quote: quote,
-    },
-    function () {}
-  );
+  $.post("/signUp", {
+    firstName: firstName,
+    lastName: lastName,
+    picture: picture,
+    description: description,
+    aboutMe: aboutMe,
+    city: city,
+    country: country,
+    balance: 0,
+    email: email,
+    password: password,
+    supporters: [],
+    quote: quote,
+  }).then(() => {
+    window.location.href = `sign-in/sign-in.js`;
+  });
 });

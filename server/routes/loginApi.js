d64c92a9b85e0e01fff300bbca7887e50a5dc065
Henry1998N <henry.nur137@gmail.com>
2023-03-22 16:17:34 +0200

    removed console log

--- server/routes/loginApi.js
+++ server/routes/loginApi.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const loginFunctions = require("../utils/loginFunc");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
-// const salt = bcrypt.genSaltSync(saltRounds);
 router.post("/login", function (req, res) {
   const { email, password } = req.body;
-  // const users = [
-  //   { email: "henry@gmail.com", password: bcrypt.hashSync("123", salt) },
-  // ];
   const isValidUser = loginFunctions.validateUser(email, password);
   isValidUser.then((result) => {
     if (result) {

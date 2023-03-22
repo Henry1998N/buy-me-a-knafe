aec496f7326d009c80292eb3a3da7c12fc60b8fa
Henry1998N <henry.nur137@gmail.com>
2023-03-22 14:20:57 +0200

    added main js file to granteeSignedIn folder

---
+++ dist/granteeSignedIn/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".animal button").on("click", function () {
+    let animal = $(this).siblings("img").attr("alt").toLowerCase();
+
+    $.ajax({
+      url: `/animals/${animal}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      success: function (response) {
+        alert(response);
+      },
+      error: function (res, status, error) {
+        alert(res.responseText);
+        location.href = "/";
+      },
+    });
+  });
+});
